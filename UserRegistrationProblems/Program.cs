6d22fe74a4159d26c4da1ec80a5adc6cd1d99a2b
Rakesh-space <rakeshsmusale03@gmail.com>
2021-11-01 16:21:27 +0530

    Adding Welcome message..

---
+++ UserRegistrationProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome User Registration Problem..");
+        }
+    }
+}
