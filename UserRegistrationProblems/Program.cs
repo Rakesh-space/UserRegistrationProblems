e30afa9471ccce7ebdac59c3050c4852bc36e14d
Rakesh-space <rakeshsmusale03@gmail.com>
2021-11-11 16:21:50 +0530

    Upadated UC2 User tacking input..

---
+++ UserRegistrationProblems/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expression Program");
+            Console.WriteLine();
+
+            Console.Write("Enter First Name : ");   //taking user input of first name
+            string firstname = Console.ReadLine();
+            ValidatingRegex.validFirstName(firstname);
+            Console.ReadLine();
+
+            Console.Write("Enter Last Name : ");   //taking user input of Last name
+            string lastname = Console.ReadLine();
+            ValidatingRegex.LastName(lastname);
+            Console.ReadLine();
+
+        }
+    }
+}
